18921373dc9fed35af22766a6c39a1be8d2c6a46
ahmadsaipullah <ahmadsaipullah911@gmail.com>
2023-12-06 20:18:03 +0700

    menambahkan migration

---
+++ database/migrations/2023_12_06_131214_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transactions_id')->constrained('transaction_details')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('products_id')->constrained('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
